f1a5627f65ca0fd4cfc846d20a09e80137a448e0
Simon <sajmon2325@gmail.com>
2021-01-20 21:52:59 +0100

    Setting up the project

---
+++ src/main/java/com/opensourcedev/eventar/EventarApplication.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.eventar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensourcedev/eventar/EventarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.eventar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
